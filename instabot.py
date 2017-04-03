e119e89c8e10f83904c1b748e4c27ab7389619e0
rishavdevgan <rishav20devgan@gmail.com>
2017-04-03 23:32:34 +0530

    Instgram fetch information

--- instabot.py
+++ instabot.py
@@ -4,20 +4,19 @@ APP_ACCESS_TOKEN = '3659496219.f4a9425.994f4f785e1542d4b933f2e19eb922d2'
 BASE_URL = 'https://api.instagram.com/v1/'
 
 def self_info():
-    #request_url = BASE_URL + '/users/self/?access_token=' + APP_ACCESS_TOKEN
     request_url = (BASE_URL + 'users/self/?access_token=%s') % (APP_ACCESS_TOKEN)
     print 'REQUESTING URL FOR DATA: ' + request_url
     my_info = requests.get(request_url).json()
     print 'MY INFO IS: \n'
-    print my_info['data']['bio']
-    print my_info['data']['website']
-    print my_info['data']['full_name']
+    print my_info
 
 self_info()
 
 print '\r\n'
 
 def get_user_by_username(insta_usernname):
+
+    insta_usernname = raw_input("Enter the username of the person you want to see the information\n")
     request_url = (BASE_URL + 'users/search?q=%s&access_token=%s') % (insta_usernname, APP_ACCESS_TOKEN)
     print  'REQUESTING URL FOR DATA : ' + request_url
 
@@ -25,4 +24,4 @@ def get_user_by_username(insta_usernname):
 
     print search_results
 
-get_user_by_username('devgan.shweta')
+get_user_by_username('insta_usernname')
