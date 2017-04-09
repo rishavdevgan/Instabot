29eede3fe955f7865535a851f8174db3eb01fe26
rishavdevgan <rishav20devgan@gmail.com>
2017-04-09 12:15:16 +0530

    Added recent posts, like posts and input from ther user for a specific task

--- instabot.py
+++ instabot.py
@@ -14,14 +14,87 @@ self_info()
 
 print '\r\n'
 
-def get_user_by_username(insta_usernname):
+insta_username = raw_input("What username do you want me to take requests for Sir ??\n")
 
-    insta_usernname = raw_input("Enter the username of the person you want to see the information\n")
-    request_url = (BASE_URL + 'users/search?q=%s&access_token=%s') % (insta_usernname, APP_ACCESS_TOKEN)
+print "What do you wish to do from the following tasks:\n" \
+      "1. Get the user's Instagram ID\n" \
+      "2. Get the user's recent posts\n" \
+      "3. Like the posts"
+
+make_a_choice = raw_input("Select from the above options\n")
+print "You have selected %s option" %(make_a_choice)
+
+
+
+def get_user_id_by_username(insta_username):
+
+    request_url = (BASE_URL + 'users/search?q=%s&access_token=%s') % (insta_username, APP_ACCESS_TOKEN)
     print  'REQUESTING URL FOR DATA : ' + request_url
 
     search_results = requests.get(request_url).json()
 
-    print search_results
+    if search_results['meta']['code'] == 200:
+        if len(search_results['data']):
+            print search_results['data'][0]['id']
+            return search_results['data'][0]['id']
+        else:
+            print 'User does not exist!'
+    else:
+        print 'Status code other than 200 was received!'
 
-get_user_by_username('insta_usernname')
+    return None
+
+
+
+def get_users_recent_posts(insta_username):
+    insta_user_id = get_user_id_by_username(insta_username)
+    request_url = (BASE_URL + 'users/%s/media/recent/?access_token=%s') % (insta_user_id, APP_ACCESS_TOKEN)
+
+    print 'REQUESTING URL FOR DATA : ' + request_url
+
+    recent_posts = requests.get(request_url).json()
+
+    if recent_posts['meta']['code'] == 200:
+        if len(recent_posts['data']):
+
+                print recent_posts['data'][0]['id']
+                return recent_posts['data'][0]['id']
+
+        else:
+            print 'No recent post by this user!'
+    else:
+        print 'Status code other than 200 was received!'
+
+
+
+def like_post_for_user(insta_username):
+    post_id = get_users_recent_posts(insta_username)
+    payload = {'access_token': APP_ACCESS_TOKEN}
+    request_url = (BASE_URL + 'media/%s/likes') % (post_id)
+
+    response_to_like = requests.post(request_url, payload).json()
+
+    print response_to_like
+
+    if response_to_like['meta']['code'] == 200:
+        print 'Liked Successfully'
+    else:
+        print 'Please try again!'
+
+
+
+if make_a_choice == "1":
+    print get_user_id_by_username(insta_username)
+
+elif make_a_choice == "2":
+    get_users_recent_posts(insta_username)
+
+elif make_a_choice == "3":
+    like_post_for_user(insta_username)
+
+else:
+    print "Select a suitable option.....................!!!!!!!!"
+
+
+
+
